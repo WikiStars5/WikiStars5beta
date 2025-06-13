357a51b85b33cd4f681dbc385f60c9a77cd56ff4
Wikibot1 <wikibot1.000@gmail.com>
2025-06-13 01:18:27 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  devIndicators: {
+    allowedDevOrigins: [
+      'https://9000-firebase-studio-1749775328349.cluster-vpxjqdstfzgs6qeiaf7rdlsqrc.cloudworkstations.dev',
+      'https://6000-firebase-studio-1749775328349.cluster-vpxjqdstfzgs6qeiaf7rdlsqrc.cloudworkstations.dev',
+      // It's possible other ports/prefixes might be used by the environment.
+      // If new warnings appear for different origins, they may need to be added here.
+    ],
+  },
 };
 
 export default nextConfig;
