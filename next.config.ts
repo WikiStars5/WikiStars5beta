cbc5f732da2457e88579322cf8331c4f98dd175c
Wikibot1 <wikibot1.000@gmail.com>
2025-06-14 17:31:47 +0000

    Modifica la aplicación para permitir que las imágenes de los perfiles de

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   devIndicators: {
