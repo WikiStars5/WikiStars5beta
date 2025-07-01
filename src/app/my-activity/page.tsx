bf7ec537ba7b29d6c3e8e94913cb5aff24638daa
Wikibot1 <wikibot1.000@gmail.com>
2025-07-01 18:11:43 +0000

    tengo duda, ahora mis sitio si esta preparado para recibir millones de v

--- src/app/my-activity/page.tsx
+++ src/app/my-activity/page.tsx
@@ -5,7 +5,8 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 /**
- * This page has been disabled due to persistent errors.
+ * This page has been disabled due to persistent errors and the removal
+ * of the "Favorites" feature, which it depended on.
  * It now redirects users to the home page.
  */
 export default function DisabledMyActivityPage() {
