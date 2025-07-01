fb719cde1eb3786ec0f2e8e6bd5459b6cae7f9ad
Wikibot1 <wikibot1.000@gmail.com>
2025-07-01 17:50:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,8 +2,10 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type, ...props }, ref) => {
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>(({ className, type, ...props }, ref) => {
     return (
       <input
         type={type}
