8ff4a18b6f503287aeb91654320f9e37bc765abc
Wikibot1 <wikibot1.000@gmail.com>
2025-07-10 16:39:59 +0000

    ya probamos dos métodos, igual no funciona, habrá un tercero?

---
+++ src/public/service-worker.js
@@ -0,0 +1,4 @@
+// This file is no longer necessary.
+// The Firebase messaging script is now directly injected into the
+// PWA's service worker via the next.config.ts file.
+// This file can be safely deleted.
