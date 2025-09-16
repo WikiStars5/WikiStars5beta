56d81434ebd9c3972b0883edbd03a37ae0ce90b1
Wikibot1 <wikibot1.000@gmail.com>
2025-09-16 17:05:29 +0000

    aca debrias especificar mejor, decir que sumado neutral, simp, fan y hat

--- src/app/figures/new-manual/page.tsx
+++ src/app/figures/new-manual/page.tsx
@@ -8,8 +8,8 @@ export default function NewManualFigurePage() {
       <CardHeader className="px-0 mb-4">
         <CardTitle className="text-2xl font-headline">Crear Nuevo Perfil Manualmente</CardTitle>
         <CardDescription>
-          Completa los detalles para la nueva figura. Este perfil deberá ser validado por la comunidad
-          al recibir suficientes votos en 5 días o será eliminado automáticamente.
+          Completa los detalles para la nueva figura. Este perfil deberá ser validado por la comunidad:
+          si la suma total de votos de actitud (Fan, Simp, Hater, Neutral) no alcanza los 1,000 en 5 días, el perfil será eliminado automáticamente.
         </CardDescription>
       </CardHeader>
       <FigureForm mode="manual" />
