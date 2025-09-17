334d355029eb6ecfd224f30fdc1e3a95a75ead56
Wikibot1 <wikibot1.000@gmail.com>
2025-09-17 22:48:16 +0000

    digo viendo de blanco los costados, se veria mejor si fuera negro

--- src/components/figures/TikTokEmbed.tsx
+++ src/components/figures/TikTokEmbed.tsx
@@ -1,15 +1,25 @@
 
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const TikTokEmbed = ({ videoId }: { videoId: string }) => {
-  if (!videoId) return null;
+  const [embedSrc, setEmbedSrc] = useState('');
+
+  useEffect(() => {
+    // This ensures window.location.hostname is only accessed on the client-side
+    // and constructs the URL with the necessary parameters for better embedding.
+    const parentDomain = window.location.hostname;
+    setEmbedSrc(`https://www.tiktok.com/embed/v2/${videoId}?refer=embed&embed_source=121374463&use_clips=false&parent_domain=${parentDomain}`);
+  }, [videoId]);
+
+
+  if (!videoId || !embedSrc) return null;
   
   return (
     <iframe
       key={videoId} // Add key to ensure re-render on videoId change
-      src={`https://www.tiktok.com/embed/v2/${videoId}`}
+      src={embedSrc}
       style={{
         width: '100%',
         height: '100%',
